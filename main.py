f77b7e083e4797d59c2ca75458153bbb1f900ee3
Dmitriy Abakumov <Abakumov.dm@yandex.ru>
2021-07-31 15:20:58 +0300

    diplom_var_1

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests
-# import time
-# from progress.bar import IncrementalBar
 
 with open('token.txt', 'r', encoding='utf-8') as file_object:
-    token = file_object.read().strip()
+    token = file_object.read()
+
+with open('ya_token.txt', 'r', encoding='utf-8') as file_object:
+    ya_token = file_object.read()
 
 class VkDownloader:
     url = 'https://api.vk.com/method/'
     def __init__(self, token, version):
         self.params = {
-            'access token': token,
+            'access_token': token,
             'v': version
         }
 
@@ -23,30 +24,25 @@ class VkDownloader:
             'count': count
         }
         req = requests.get(download_photos_url, params={**self.params, **download_photos_params}).json()
-        print(req)
+        return req
 
     def upload(self, ya_token, owner_id):
         url = "https://cloud-api.yandex.net:443/v1/disk/resources/upload"
         headers = {'Authorization': 'OAuth {}'.format(ya_token)}
-        # mylist = [1, 2, 3, 4, 5, 6, 7, 8]
-        # bar = IncrementalBar('Countdown', max=len(mylist))
-        # for step in mylist:
-        #     bar.next()
-        #     step.sleep(1)
         for item in self.download_photos(owner_id)["response"]["items"]:
-            photo_name = item["likes"]["count"]
-            params = {"path": f"vk_photos/{photo_name}.jpg"}
+            photo_name_1 = item["likes"]["count"]
+            photo_name_2 = item["id"]
+            photo_name = f'{photo_name_1} _ {photo_name_2}'
+            params = {"path": f"vk_photos/{photo_name}.jpg", "overwrite": "true"}
             response = requests.get(url=url, headers=headers, params=params)
-            # return pprint(response.json())
             for var_photo in item["sizes"]:
-                if var_photo["type"] == 'w':
-                    photo_to_upload = var_photo["url"]
-                resp = requests.put(response.json()['href'], data=open(photo_to_upload, 'rb'))
-                if resp.status_code == 201:
-                    return print("Файл успешно загружен")
-            # print(resp.status_code)
-        # bar.finish()
+                if var_photo["type"] == 'y':
+                    photo_to_upload = requests.get(var_photo["url"]).content
+                    resp = requests.put(response.json()['href'], data=photo_to_upload)
+                    if resp.status_code == 201:
+                        print("Файл успешно загружен")
+
 download_vk = VkDownloader(token, 5.131)
 download_vk.download_photos(552934290)
-
+download_vk.upload(ya_token, 552934290)
 
